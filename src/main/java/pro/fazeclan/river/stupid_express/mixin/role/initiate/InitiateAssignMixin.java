509eaa15d081294f6c9be2d727c9f412e7042a28
haiman <83174104+haiman233@users.noreply.github.com>
2026-01-20 00:23:41 +0800

    修复纵火犯被改成初学者时，身上会有汽油桶和打火机

--- src/main/java/pro/fazeclan/river/stupid_express/mixin/role/initiate/InitiateAssignMixin.java
+++ src/main/java/pro/fazeclan/river/stupid_express/mixin/role/initiate/InitiateAssignMixin.java
@@ -1,5 +1,6 @@
 package pro.fazeclan.river.stupid_express.mixin.role.initiate;
 
+import dev.doctor4t.trainmurdermystery.api.TMMItems;
 import dev.doctor4t.trainmurdermystery.cca.GameWorldComponent;
 import net.fabricmc.loader.impl.util.log.Log;
 import net.fabricmc.loader.impl.util.log.LogCategory;
@@ -10,12 +11,25 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import pro.fazeclan.river.stupid_express.constants.SEItems;
 import pro.fazeclan.river.stupid_express.constants.SERoles;
 
 import java.util.List;
 
 @Mixin(ModdedMurderGameMode.class)
 public class InitiateAssignMixin {
+
+    private static void clearModItems(ServerPlayer player) {
+        for (int i = 0; i < player.getInventory().getContainerSize(); i++) {
+            var item = player.getInventory().getItem(i);
+            // 只清除模组物品：刀、汽油桶、打火机，保留钥匙和信件
+            if (item.is(TMMItems.KNIFE) ||
+                item.is(SEItems.JERRY_CAN) ||
+                item.is(SEItems.LIGHTER)) {
+                player.getInventory().setItem(i, net.minecraft.world.item.ItemStack.EMPTY);
+            }
+        }
+    }
 
     @Inject(
             method = "assignCivilianReplacingRoles",
@@ -42,7 +56,9 @@ public class InitiateAssignMixin {
             if (!gameWorldComponent.isInnocent(player)
                     && !gameWorldComponent.canUseKillerFeatures(player)
                     && !gameWorldComponent.isRole(player, SERoles.INITIATE)) {
+                // 只清除模组物品，保留钥匙和信件              
                 gameWorldComponent.addRole(player, SERoles.INITIATE);
+                clearModItems(player);
                 Log.info(LogCategory.GENERAL, player.getScoreboardName() + " -> " + SERoles.INITIATE.identifier().toString());
                 break;
             }
