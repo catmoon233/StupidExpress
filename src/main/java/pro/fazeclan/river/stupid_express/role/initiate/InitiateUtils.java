88ba6f254509a87d7a609af134e3cfab4e3fadf9
haiman <83174104+haiman233@users.noreply.github.com>
2026-02-09 01:40:04 +0800

    场上只有单个初学者时，变成失忆患者触发角色分配事件

--- src/main/java/pro/fazeclan/river/stupid_express/role/initiate/InitiateUtils.java
+++ src/main/java/pro/fazeclan/river/stupid_express/role/initiate/InitiateUtils.java
@@ -1,13 +1,17 @@
 package pro.fazeclan.river.stupid_express.role.initiate;
 
+import dev.doctor4t.trainmurdermystery.TMM;
 import dev.doctor4t.trainmurdermystery.cca.GameTimeComponent;
 import dev.doctor4t.trainmurdermystery.cca.GameWorldComponent;
 import dev.doctor4t.trainmurdermystery.game.GameConstants;
 import dev.doctor4t.trainmurdermystery.index.TMMItems;
+import dev.doctor4t.trainmurdermystery.util.AnnounceWelcomePayload;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.server.MinecraftServer;
 
 import net.minecraft.server.level.ServerPlayer;
+import org.agmas.harpymodloader.events.ModdedRoleAssigned;
 import pro.fazeclan.river.stupid_express.constants.SEItems;
 import pro.fazeclan.river.stupid_express.constants.SERoles;
 import pro.fazeclan.river.stupid_express.utils.RoleUtils;
@@ -80,6 +84,11 @@ public class InitiateUtils {
                     ServerPlayer initiate = initiates.get(0);
                     clearModItems(initiate);
                     RoleUtils.changeRole(initiate, SERoles.AMNESIAC);
+                    ModdedRoleAssigned.EVENT.invoker().assignModdedRole(initiate, SERoles.AMNESIAC);
+                    TMM.REPLAY_MANAGER.recordPlayerRoleChange(initiate.getUUID(), SERoles.INITIATE, SERoles.AMNESIAC);
+                    ServerPlayNetworking.send(initiate,
+                            new AnnounceWelcomePayload(gameWorldComponent.getRole(initiate).getIdentifier().toString(),
+                                    gameWorldComponent.getAllKillerTeamPlayers().size(), 0));
                 }
             }
         });
